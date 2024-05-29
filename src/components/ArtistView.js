0864c21ee4122e990fab9c496ec3ae5d40e0892f
suttonj07 <suttonj07@gmail.com>
2024-05-29 19:48:13 -0400

    added server

--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -1,17 +1,38 @@
-import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
 
 function ArtistView() {
     const { id } = useParams()
     const [ artistData, setArtistData ] = useState([])
 
+    useEffect(() => {
+        const API_URL = `http://localhost:4000/album/${id}`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setArtistData(resData.results)
+        }
+        fetchData()
+    }, [id])
+
+    const justAlbums = artistData.filter(entry => entry.collectionType === 'Album')
+
+    const renderAlbums = justAlbums.map((album, i) => {
+        return (
+            <div key={i}>
+                <p>{album.collectionName}</p>
+            </div>
+        )
+    })
+    
     return (
         <div>
             <h2>The id passed was: {id}</h2>
             <p>Artist Data Goes Here!</p>
+            {renderAlbums}
+            <Link to={'/'}>Home</Link>
         </div>
     )
 }
 
-
-export default ArtistView
+export default ArtistView;
